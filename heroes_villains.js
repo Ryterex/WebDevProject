e104e44b78eaa2500423a09d878a0dfed7bfb299
Caleb Obeng <cobeng@stevens.edu>
2019-05-13 14:27:46 -0400

    start of the database

---
+++ heroes_villains.js
@@ -0,0 +1,24 @@
+module.exports = [
+    {
+        name: "Orm Marius",
+        altEgo: "Ocean Master",
+        universe: "DCEU",
+        films: ["Aquaman"],
+        nemeses: ["Aquaman"],
+        powers: ["skilled combatant", "atlantean strength", "heightened senses", "can survive on land or in sea", "master strategist"],
+        background: "Serving as Atlantis' king, Ocean Master wanted to unite the four remaining kingdoms of Atlantis in a war against the surface. Orm's aspirations led to his advisor Vulko and fiance Mera plotting against him with Aquaman (Orm's older half-brother) to prevent a catastrophic war between land and sea.",
+        movieLook: "movieoceanmaster.jpg",
+        comicLook: "comicoeanmaster.jpg"
+    },
+    {
+        name: "Tony Stark",
+        altEgo: "Iron Man",
+        universe: "MCU",
+        films: ["Iron Man", "The Incredible Hulk", "Iron Man 2", "The Avengers", "Iron Man 3", "Avengers: Age of Ultron", "Captain America: Civil War", "Spider-Man: Homecoming", "Avengers: Infinity War", "Avengers: Endgame"],
+        nemeses: ["Iron Monger", "Whiplash", "Thanos", "Justin Hammer", "Aldrich Killian"],
+        powers: ["Exo-suit", "Genius", "Scientist"],
+        background: "Anthony Edward \"Tony\" Stark was a billionaire industrialist, a founding member of the Avengers, and the former CEO of Stark Industries, a company originally started by his father, Howard Stark. A brash but brilliant inventor, Stark was self-described as a genius, billionaire, playboy and philanthropist. With his great wealth and exceptional technical knowledge, Stark was one of the world's most powerful men, and enjoyed the playboy lifestyle for many years until he was kidnapped by the Ten Rings in Afghanistan, while demonstrating a fleet of Jericho missiles. With his life on the line, Stark created an armored suit which he used to escape his captors, returning home and becoming known as an Iron Man, battling against terrorists as well as Obadiah Stane. Stark enjoyed the fame that came with his new secret identity and decided to share it with the world, publicly announcing himself as Iron Man.",
+        movieLook: "movieironman.jpg",
+        comicLook: "comicironman.jpg"
+    }
+];
