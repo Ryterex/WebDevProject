418a1aff49eb982c480ac29c55fa483a83f46d83
Caleb Obeng <cobeng@stevens.edu>
2019-05-14 21:25:16 -0400

    heroes updates

---
+++ data/heroes_villains.js
@@ -0,0 +1,122 @@
+module.exports = [
+    {
+    	_id: 0,
+        name: "Orm Marius",
+        altEgo: "Ocean Master",
+        universe: "DCEU",
+        films: ["Aquaman"],
+        nemeses: ["Aquaman"],
+        powers: ["skilled combatant", "atlantean strength", "heightened senses", "can survive on land or in sea", "master strategist"],
+        background: "Serving as Atlantis' king, Ocean Master wanted to unite the four remaining kingdoms of Atlantis in a war against the surface. Orm's aspirations led to his advisor Vulko and fiance Mera plotting against him with Aquaman (Orm's older half-brother) to prevent a catastrophic war between land and sea.",
+        movieLook: "movieoceanmaster.jpg",
+        comicLook: "comicoeanmaster.jpg"
+    },
+    {
+    	_id: 1,
+        name: "Tony Stark",
+        altEgo: "Iron Man",
+        universe: "MCU",
+        films: ["Iron Man", "The Incredible Hulk", "Iron Man 2", "The Avengers", "Iron Man 3", "Avengers: Age of Ultron", "Captain America: Civil War", "Spider-Man: Homecoming", "Avengers: Infinity War", "Avengers: Endgame"],
+        nemeses: ["Iron Monger", "Whiplash", "Thanos", "Justin Hammer", "Aldrich Killian"],
+        powers: ["Exo-suit", "Genius", "Scientist"],
+        background: "Anthony Edward \"Tony\" Stark was a billionaire industrialist, a founding member of the Avengers, and the former CEO of Stark Industries, a company originally started by his father, Howard Stark. A brash but brilliant inventor, Stark was self-described as a genius, billionaire, playboy and philanthropist. With his great wealth and exceptional technical knowledge, Stark was one of the world's most powerful men, and enjoyed the playboy lifestyle for many years until he was kidnapped by the Ten Rings in Afghanistan, while demonstrating a fleet of Jericho missiles. With his life on the line, Stark created an armored suit which he used to escape his captors, returning home and becoming known as an Iron Man, battling against terrorists as well as Obadiah Stane. Stark enjoyed the fame that came with his new secret identity and decided to share it with the world, publicly announcing himself as Iron Man.",
+        movieLook: "movieironman.jpg",
+        comicLook: "comicironman.jpg"
+    },
+    {
+    	_id: 2,
+        name: "Peter Parker",
+        altEgo: "Spider-Man",
+        universe: "MCU",
+        films: ["Captain America: Civil War", "Spider-Man: Homecoming", "Avengers: Infinity War", "Avengers: Endgame", "Spider-Man: Far From Home"],
+        nemeses: ["Vulture"],
+        powers: ["High-tech suit", "Super Vision", "Spider Senses", "Spider Webs", "Sticking to walls", "Durability"],
+        background: "Peter Benjamin Parker is a high school student who gained spider-like abilities, fighting crime across New York City as the alter ego of Spider-Man, hoping some day he'll live up to his heroes in the Avengers. While Parker juggled all his continued superhero duties with the demands of his high-school life, he was approached by Tony Stark who recruited Spider-Man to join the Avengers Civil War, putting Spider-Man into the brief conflict with his personal hero, Captain America. Parker then agreed and was given his new suit as well as brand new Stark technology in exchange for all his help.",
+        movieLook: "moviespiderman.jpg",
+        comicLook: "comicspiderman.jpg"
+    },
+    {
+    	_id: 3,
+        name: "Steve Rogers",
+        altEgo: "Captain America",
+        universe: "MCU",
+        films: ["Captain America: The First Avenger", "The Avengers", "Captain America: The Winter Soldier", "Avengers: Age of Ultron", "Captain America: Civil War", "Avengers: Infinity War", "Avengers: Endgame", "Spider-Man: Far From Home"],
+        nemeses: ["Red Skull", "Thanos"],
+        powers: ["Super-human abilities", "Vibranium Shield", "Worthy"],
+        background: "Captain Steven Grant \"Steve\" Rogers is a World War II veteran, a founding member of the Avengers, and Earth's first known superhero. Rogers had suffered from numerous health problems, and upon America's entry into World War II, was rejected from military service despite several attempts to enlist. Rogers was the only recipient of the Super Soldier Serum developed by Abraham Erskine under the Strategic Scientific Reserve. Determined to serve, Rogers ultimately volunteered for the Project Rebirth, which enhanced the frail Rogers' body to the peak of human physicality. Mistrusted by Colonel Chester Phillips, the head of the SSR, Rogers was relegated to propaganda campaigns, and was given the new moniker of Captain America. Rogers later joined the war with a combat role after he single-handedly liberated captured Allied prisoners of war.",
+        movieLook: "moviecaptainamerica.jpg",
+        comicLook: "comiccaptainamerica.jpg"
+    },
+    {
+    	_id: 4,
+        name: "Zod",
+        altEgo: "General Zod",
+        universe: "DCEU",
+        films: ["Man of Steel", "Batman v Superman: Dawn of Justice"],
+        nemeses: ["Superman"],
+        powers: ["Kryptonian Might","Intelligence", "Skilled Combatant"],
+        background: "General Zod was a Kryptonian war criminal and former leader of the Warrior Guild who later founded the Sword of Rao, leading them during the events of the Kryptonian Civil War. Managing to survive the destruction of his native planet of Krypton and escaping from his Phantom Zone imprisonment, he viewed the purpose of his life to be the successful re-establishment of Krypton on another planet. General Zod was the first great enemy of the hero Superman, who ultimately defeated and killed him in combat after a prolonged devastating duel in the heart of Metropolis, during the Battle of Metropolis Zod notably killed many Wayne Enterprises employees before going down.",
+        movieLook: "moviegeneralzod.jpg",
+        comicLook: "comicgeneralzod.jpg"
+    },
+    {
+    	_id: 5,
+        name: "Bruce Wayne",
+        altEgo: "Batman",
+        universe: "DCEU",
+        films: ["Batman v Superman: Dawn of Justice", "Suicide Squad", "Justice League"],
+        nemeses: ["Joker", "Penguin"],
+        powers: ["Bat Suit", "Batarang", "Inventor", "Genius"],
+        background: "Bruce Wayne is the CEO of Wayne Enterprises and the vigilante operating in Gotham City, New Jersey known as the Batman. After witnessing the murder of his parents at the hands of a mugger as a child, Bruce waged a war on crime in Gotham City for over 20 years before the Battle of Metropolis. Over the years, Bruce encountered numerous foes, most notably the Penguin and the Joker. He also mentored Jason Todd to aid him as Robin, though Todd was eventually killed by the Joker and his doctor-turned-partner, Harley Quinn.",
+        movieLook: "moviebatman.jpg",
+        comicLook: "comicbatman.jpg"
+    },
+    {
+    	_id: 6,
+        name: "(Unknown)",
+        altEgo: "Joker",
+        universe: "DCEU",
+        films: ["Suicide Squad"],
+        nemeses: ["Batman"],
+        powers: ["Comedian"],
+        background: "The Joker is an infamously extreme and formidable psychopathic crime lord from Gotham City, being at the very top of the city's criminal underworld, greatly feared by other crime bosses. Devoid of empathy or pity for anybody taking part in his wicked schemes either by intimidation or fraud, the Joker does, however, adore his girlfriend and partner in crime Harley Quinn, doing his utmost to rescue her from the Suicide Squad and clutches of Amanda Waller, with eventual success. The Joker is also the long-time arch nemesis of Batman, and the murderer of the latter's protege Robin along with Harley Quinn.",
+        movieLook: "moviejoker.jpg",
+        comicLook: "comicjoker.jpg"
+    },
+    {
+    	_id: 7,
+        name: "Kal-El",
+        altEgo: "Superman",
+        universe: "DCEU",
+        films: ["Man of Steel", "Batman v Superman: Dawn of Justice", "Justice League", "Shazam!"],
+        nemeses: ["Lex Luther", "General Zod", "Doomsday"],
+        powers: ["Kryptonian Might", "Super Speed", "Flight", "Frost Breath", "Superhuman Senses"],
+        background: "Clark Joseph Kent, born with the Kryptonian name Kal-El, is an incredibly powerful alien superhero of the House of El, one of the extremely few survivors of the destruction of Krypton, and an investigative reporter of the Daily Planet. In addition, Kal-El is the first Kryptonian of natural birth in centuries and the host of the Growth Codex and the leader of the Justice League.",
+        movieLook: "moviesuperman.jpg",
+        comicLook: "comicsuperman.jpg"
+    },
+    {
+    	_id: 8,
+        name: "King T'Challa",
+        altEgo: "Black Panther",
+        universe: "MCU",
+        films: ["Captain America: Civil War", "Black Panther", "Avengers: Infinity War", "Avengers: Endgame"],
+        nemeses: ["Killmonger"],
+        powers: ["Vibranium Suit", "Panther-like abilities"],
+        background: "T'Challa is the King of the African nation of Wakanda, and the son of T'Chaka and Ramonda. As the Wakandan monarch, he was also the former holder of the Black Panther mantle. After his father was killed in a bombing attack orchestrated by Helmut Zemo, the new monarch set out to kill the Winter Soldier, who was widely believed to be responsible for the attack. After being drawn into the Avengers Civil War and siding with Iron Man, T'Challa learned the truth about Zemo's deception and decided to help the Winter Soldier by granting him asylum in Wakanda and helping him get a cure for his HYDRA mental programming.",
+        movieLook: "movieblackpanther.jpg",
+        comicLook: "comicblackpanther.jpg"
+    },
+    {
+    	_id: 9,
+        name: "Thor",
+        altEgo: "Thor",
+        universe: "MCU",
+        films: ["Thor", "The Avengers", "Thor: The Dark World", "Avengers: Age of Ultron", "Thor: Ragnarok", "Avengers: Infinity War", "Avengers: Endgame"],
+        nemeses: ["Loki", "Thanos"],
+        powers: ["God of Thunder"],
+        background: "Thor Odinson is the former king of Asgard and New Asgard, a founding member of the Avengers, and the God of Thunder. When his irresponsible and impetuous behavior reignited a conflict between Asgard and Jotunheim, Thor was denied the right to become king, stripped of his power, and banished by his father Odin to Earth. While exiled on Earth, Thor learned humility, finding love with Jane Foster, and helping to save his new friends from a destructive threat sent by his adoptive brother Loki. Due to his selfless act of sacrifice, Thor redeemed himself in his father's eyes and was granted his power once more, which he then used to defeat Loki's schemes of genocide.",
+        movieLook: "moviethor.jpg",
+        comicLook: "comicthor.jpg"
+    }
+];
